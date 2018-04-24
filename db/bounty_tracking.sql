d9a5c25b94aee2e0491777f0fce2d12d19d8d37f
Paul <rpmstevenson@gmail.com>
2018-04-24 15:31:36 +0100

    set-up complete

---
+++ db/bounty_tracking.sql
@@ -0,0 +1,8 @@
+DROP TABLE IF EXISTS bounties;
+CREATE TABLE bounties(
+  id SERIAL4 PRIMARY KEY,
+  name VARCHAR(255),
+  bounty_value INT2,
+  last_known_location VARCHAR(255),
+  favourite_weapon VARCHAR(255)
+);
--- models/bounty.rb
+++ models/bounty.rb
@@ -2,6 +2,7 @@ require('pg')
 
 class Bounty
 
+attr_accessor :name, :bounty_value, :last_known_location, :favourite_weapon
 
   def initialize(options)
     @id = options["id"].to_i if options["id"]
@@ -9,5 +10,5 @@ class Bounty
     @bounty_value = options["bounty_value"].to_i
     @last_known_location = options["last_known_location"]
     @favourite_weapon = options["favourite_weapon"]
-  end 
+  end
 end
